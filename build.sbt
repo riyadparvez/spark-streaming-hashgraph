1e6bff6d03c9a0e4e0423ba208f17f78eaddff86
Riyad Parvez <riyad.parvez@gmail.com>
2016-04-12 16:48:13 -0400

    Running

--- build.sbt
+++ build.sbt
@@ -5,7 +5,17 @@ lazy val root = (project in file(".")).
     scalaVersion := "2.10.6"
   )
 
+mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+   {
+    case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+    case x => MergeStrategy.first
+   }
+}
+
 libraryDependencies += "commons-io" % "commons-io" % "2.4"
+libraryDependencies += "com.google.code.gson" % "gson" % "2.3"
+libraryDependencies += "org.twitter4j" % "twitter4j-core" % "3.0.3"
+libraryDependencies += "commons-cli" % "commons-cli" % "1.2"
 
 libraryDependencies += "org.apache.spark" % "spark-streaming_2.10"         % "1.6.1" % "provided"
-libraryDependencies += "org.apache.spark" % "spark-streaming-twitter_2.10" % "1.6.1" % "provided"
+libraryDependencies += "org.apache.spark" % "spark-streaming-twitter_2.10" % "1.6.1"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,30 @@
 import org.apache.spark._
 import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter.TwitterUtils
 
-object Main extends App {
-  val conf = new SparkConf().setAppName("streaming-hashgraph").setMaster("local[*]")
+import com.google.gson.Gson
+
+object Main {
+  private var gson = new Gson()
+
+  def main(args: Array[String]) {
+    Utils.parseCommandLineWithTwitterCredentials(args)
+      
+    val conf = new SparkConf().setAppName("streaming-hashgraph")
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(60))
   
+    val tweetStream = TwitterUtils.createStream(ssc, Utils.getAuth)
+      .map(gson.toJson(_))
+    
+    tweetStream.foreachRDD((rdd, time) => {
+      val count = rdd.count()
+      if (count > 0) {
+        val outputRDD = rdd.repartition(4)
+        outputRDD.saveAsTextFile("/tmp" + "/tweets_" + time.milliseconds.toString)
+      }
+    })
+    ssc.start()
+    ssc.awaitTermination()
+  }
 }
